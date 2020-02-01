b4ed86f54cbb01328d49ec5e19c3b813eb85581f
Invalid <zappeln@gmail.com>
2020-02-01 11:35:53 -0600

    implemented check for availability of position on board; removed old code

--- main.cpp
+++ main.cpp
@@ -2,24 +2,27 @@
  * Program 2: Cuatro
  *
  * Class: CS 141, Spring 2020. Tue 4pm lab
- * System: CLion on Windows 10
+ * System: CLion on Windows 10 & CLion on Mac
  * Author: Shawn Klein
  *
  * ---------------------------------------------
  */
 #include <iostream>
-//#include <optional>
-#include <map>
-#include <regex>
 #include <string>
 
 using namespace std;
 
 // these are my global variables
+const string constCurlies = "oOcC";
+const string constStraights = "iIzZ";
+
+string avlTkns = "OOCCIIZZoocciizz";
+bool activeGame = true;
+
 char b1 = '.',  b2 = '.',  b3 = '.',  b4 = '.',  b5 = '.',  b6 = '.',  b7 = '.',  b8 = '.';
 char b9 = '.', b10 = '.', b11 = '.', b12 = '.', b13 = '.', b14 = '.', b15 = '.', b16 = '.';
-string t = "OOCCIIZZoocciizz";
-bool activeGame = true;
+
+
 
 void displayBoard() {
 
@@ -29,10 +32,10 @@ void displayBoard() {
          << b9  << ' ' << b10 << ' ' << b11 << ' ' << b12 << " |  9 10 11 12\n      | "
          << b13 << ' ' << b14 << ' ' << b15 << ' ' << b16 << " | 13 14 15 16" << endl;
     cout << "       ---------\n      Pieces:     Curved Straight"<< endl;
-    cout << "            Upper: " << t.at(0) << t.at(1) << "/" << t.at(2) << t.at(3)
-         << "  " << t.at(4) << t.at(5) << "/" << t.at(6) << t.at(7) << endl;
-    cout << "            Lower: " << t.at(8) << t.at(9) << "/" << t.at(10) << t.at(11)
-         << "  " << t.at(12) << t.at(13) << "/" << t.at(14) << t.at(15) << endl;
+    cout << "            Upper: " << avlTkns.at(0) << avlTkns.at(1) << "/" << avlTkns.at(2) << avlTkns.at(3)
+         << "  " << avlTkns.at(4) << avlTkns.at(5) << "/" << avlTkns.at(6) << avlTkns.at(7) << endl;
+    cout << "            Lower: " << avlTkns.at(8) << avlTkns.at(9) << "/" << avlTkns.at(10) << avlTkns.at(11)
+         << "  " << avlTkns.at(12) << avlTkns.at(13) << "/" << avlTkns.at(14) << avlTkns.at(15) << endl;
     cout << "                Vowel/Consonant"<< endl;
 }
 
@@ -47,31 +50,52 @@ void isTokenAvailable (char t) {
     }
 }
 
-void isPositionAvailable() {
-    //cout << "Value is: " << n << endl;
+bool isPositionAvailable(int i) {
+    switch (i-48) {
+        case  1: return b1  == '.' ? true : false;
+        case  2: return b2  == '.' ? true : false;
+        case  3: return b3  == '.' ? true : false;
+        case  4: return b4  == '.' ? true : false;
+        case  5: return b5  == '.' ? true : false;
+        case  6: return b6  == '.' ? true : false;
+        case  7: return b7  == '.' ? true : false;
+        case  8: return b8  == '.' ? true : false;
+        case  9: return b9  == '.' ? true : false;
+        case 10: return b10 == '.' ? true : false;
+        case 11: return b11 == '.' ? true : false;
+        case 12: return b12 == '.' ? true : false;
+        case 13: return b13 == '.' ? true : false;
+        case 14: return b14 == '.' ? true : false;
+        case 15: return b15 == '.' ? true : false;
+        case 16: return b16 == '.' ? true : false;
+    }
 }
 
 bool curIsFound(char c) {
-    const string curlies = "oOcC";
-    return curlies.find(c) != string::npos;
+    return constCurlies.find(c) != string::npos;
 }
 
 bool strIsFound(char c) {
-    const string straights = "iIzZ";
-    return straights.find(c) != string::npos;
+    return constStraights.find(c) != string::npos;
 }
 
 void checkCombo(char a,char b,char c,char d) {
     if (curIsFound(a) && curIsFound(b) && curIsFound(c) && curIsFound(d)){
         cout << "FOUND CURLY COMBO" << endl;
+        activeGame = false;
     }
+
     if (strIsFound(a) && strIsFound(b) && strIsFound(c) && strIsFound(d)){
         cout << "FOUND STRAIGHT COMBO" << endl;
+        activeGame = false;
     }
 }
 
 
 void checkForWin() {
+    // test function
+    //checkCombo('z' ,'z' ,'z' ,'z'); // REMOVE ME
+
     // check quadrants
     checkCombo( b1, b2, b5, b6);
     checkCombo( b3, b4, b7, b8);
@@ -99,7 +123,8 @@ void checkForWin() {
 int main() {
     bool activeGame = true;
     char userToken;
-    string userPosition;
+    char userPosition;
+    int idx;
 
     cout << "Welcome to the game of Cuatro!" << endl;
 
@@ -110,19 +135,15 @@ int main() {
         //cin >> userToken;
         //cin >> userPosition;
         userToken = 'I';
-        userPosition = "5";
+        userPosition = '1';
 
-        isPositionAvailable();
-        if (true) {
-            int i = t.find(userToken);
-            cout << "tokens found: " << i << endl;
-            if (i > -1 ){
+        if ((isPositionAvailable(userPosition)) == 1) {
+            idx = avlTkns.find(userToken);
+            if (idx > -1 ){
                 cout << "SUCCESSFUL TOKEN!" << endl;
-                t.replace(i,1," ");
-                //b1+b2+b3+b4
+                avlTkns.replace(idx ,1," ");
 
-                //cout << "compare: " << (curlies.compare(reinterpret_cast<const char *>('o' + 'c' + 'O' + 'C')) > 0) << endl;
-                checkCombo('z' ,'z' ,'z' ,'z');
+                checkForWin();
             } else {
                 cout << "BAD TOKEN!" << endl;
             }
@@ -130,13 +151,9 @@ int main() {
             cout << "Position full, try again" << endl;
         }
         //displayBoard();
-        if (userToken == 'q' || true || t == "                ") {
-            int hexVal;
-            //stringstream strm;
-            //strm << "                ";
-            //strm >> hex >> hexVal;
-            //stoi(s, 0, 16)
-            cout << "HEX: " << strtol("                ", 0, 16) << endl;
+        if (userToken == 'q' || true || avlTkns == "                ") {
+            //cout << "TEST 16 SPACES: " << (avlTkns > 512) << endl;
+
             activeGame = false;
         }
 
