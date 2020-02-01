abcd62976115c47dbc35888e0f01b1793b568977
Invalid <zappeln@gmail.com>
2020-02-01 10:15:21 -0600

    working methods to check for win

--- cmake-build-debug/CMakeFiles/Cuatro.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Cuatro.dir/cmake_clean.cmake
@@ -1,7 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Cuatro.dir/main.cpp.o"
-  "Cuatro"
+  "CMakeFiles/Cuatro.dir/main.cpp.obj"
+  "Cuatro.exe"
+  "Cuatro.exe.manifest"
   "Cuatro.pdb"
+  "libCuatro.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 //#include <optional>
 #include <map>
 #include <regex>
+#include <string>
 
 using namespace std;
 
@@ -27,21 +28,21 @@ void displayBoard() {
          << b5  << ' ' << b6  << ' ' << b7  << ' ' << b8  << " |  5  6  7  8\n      | "
          << b9  << ' ' << b10 << ' ' << b11 << ' ' << b12 << " |  9 10 11 12\n      | "
          << b13 << ' ' << b14 << ' ' << b15 << ' ' << b16 << " | 13 14 15 16" << endl;
-    cout << "       ---------" << endl;
-    cout << "      Pieces:     Curved Straight"<< endl;
+    cout << "       ---------\n      Pieces:     Curved Straight"<< endl;
     cout << "            Upper: " << t.at(0) << t.at(1) << "/" << t.at(2) << t.at(3)
          << "  " << t.at(4) << t.at(5) << "/" << t.at(6) << t.at(7) << endl;
-    cout << "            Lower: oo/cc  ii/zz"<< endl;
+    cout << "            Lower: " << t.at(8) << t.at(9) << "/" << t.at(10) << t.at(11)
+         << "  " << t.at(12) << t.at(13) << "/" << t.at(14) << t.at(15) << endl;
     cout << "                Vowel/Consonant"<< endl;
 }
 
 void isTokenAvailable (char t) {
 
     for (int i=1; i <= 16; ++i) {
-        optional<char> 'b' + to_string(i);
-        if input == 'b' + to_string(i) {
-            b + i -> variable
-        }
+        //optional<char> 'b' + to_string(i);
+        //if (input == 'b' + to_string(i)) {
+        //    b + i -> variable
+        //}
         //create(false).value_or("empty")
     }
 }
@@ -50,32 +51,46 @@ void isPositionAvailable() {
     //cout << "Value is: " << n << endl;
 }
 
-void checkCombo(char a,char b,char c,char d) {
-    // implement regex to check for matching combos
-    activeGame = regex? false : activeGame;
+bool curIsFound(char c) {
+    const string curlies = "oOcC";
+    return curlies.find(c) != string::npos;
 }
 
-// 1  2  3  4
-// 5  6  7  8
-// 9 10 11 12
-//13 14 15 16
+bool strIsFound(char c) {
+    const string straights = "iIzZ";
+    return straights.find(c) != string::npos;
+}
+
+void checkCombo(char a,char b,char c,char d) {
+    if (curIsFound(a) && curIsFound(b) && curIsFound(c) && curIsFound(d)){
+        cout << "FOUND CURLY COMBO" << endl;
+    }
+    if (strIsFound(a) && strIsFound(b) && strIsFound(c) && strIsFound(d)){
+        cout << "FOUND STRAIGHT COMBO" << endl;
+    }
+}
+
 
 void checkForWin() {
+    // check quadrants
     checkCombo( b1, b2, b5, b6);
     checkCombo( b3, b4, b7, b8);
     checkCombo( b9,b10,b13,b14);
     checkCombo(b11,b12,b15,b16);
 
+    // check rows
     checkCombo( b1, b2, b3, b4);
     checkCombo( b5, b6, b7, b8);
     checkCombo( b9,b10,b11,b12);
     checkCombo(b13,b14,b15,b16);
 
+    // check columns
     checkCombo(b1,b5,b9,b13);
     checkCombo(b2,b6,b10,b14);
     checkCombo(b3,b7,b11,b15);
     checkCombo(b4,b8,b12,b16);
 
+    // check cross
     checkCombo(b1,b6,b11,b16);
     checkCombo(b4,b7,b10,b13);
 }
@@ -104,14 +119,17 @@ int main() {
             if (i > -1 ){
                 cout << "SUCCESSFUL TOKEN!" << endl;
                 t.replace(i,1," ");
-                //m["b" + userPosition] = userToken;
+                //b1+b2+b3+b4
+
+                //cout << "compare: " << (curlies.compare(reinterpret_cast<const char *>('o' + 'c' + 'O' + 'C')) > 0) << endl;
+                checkCombo('z' ,'z' ,'z' ,'z');
             } else {
                 cout << "BAD TOKEN!" << endl;
             }
         } else {
             cout << "Position full, try again" << endl;
         }
-        displayBoard();
+        //displayBoard();
         if (userToken == 'q' || true || t == "                ") {
             int hexVal;
             //stringstream strm;
@@ -126,6 +144,16 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
 
 
 // " " is 32
